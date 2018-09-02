6f2d96fc32fc3769ecbc32bdc06034620dc53450
mthlongwane <hlnmzw001@myuct.ac.za>
2018-09-02 11:23:24 +0200

    server and init HTML serve

---
+++ server/server.js
@@ -0,0 +1,20 @@
+//node module is called path. 
+const path  = require ('path');
+const publicPath = path.join(__dirname, '../public') // this is better that ../../ because it makes issues.
+const port = process.env.port || 3000
+
+const express =  require('express');
+
+var app = express()
+
+
+app.use(express.static(publicPath));
+/*
+app.get('/',(req, res) =>{
+    res.send("Awe")
+})
+*/
+
+app.listen(port, () => {
+    console.log(`server up on port ${port}`);
+})
