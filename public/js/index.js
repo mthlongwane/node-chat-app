567e87a353b10a4a356e1a45cb29eb451d2d5e6a
mthlongwane <hlnmzw001@myuct.ac.za>
2018-09-14 16:44:50 +0200

    UI mods

--- public/js/index.js
+++ public/js/index.js
@@ -28,6 +28,16 @@ socket.on('newMessage', function(message){
 
     jQuery('#messages').append(li);
 })
+
+socket.on('newLocationMessage', function(message){
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target ="_blank">My current Location</a>');
+    li.text(`${message.from}:`);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+})
+
 /*
 socket.emit('createMessage', {
     from: 'Frank',
@@ -38,10 +48,31 @@ socket.emit('createMessage', {
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
+
     socket.emit('createMessage', {
         from: 'user',
         text: jQuery('[name=message]').val()
     }, function(response){ // this is waiting for the socket callback response
         console.log("Server response:", response)
+    });
+})
+
+var locationButton = jQuery('#send-Location') ;
+
+locationButton.on('click', function(){
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported by browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition( function(position){
+        //console.log(position);
+        
+        socket.emit('createLocationMessage',{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+        
+    }, function(){
+        alert('Unable to fetch location.')
     });
 })
